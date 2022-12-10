90e996cedcd20ad04f72ee927d0cea58c28a9886
jawwadurrehman <91111174+jawwadurrehman@users.noreply.github.com>
2022-12-10 12:46:19 +0500

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Button, View } from "react-native";
+import { Benchmark, BenchmarkType } from "react-component-benchmark";
+
+import Animated from "react-native-reanimated";
+// import { Animated } from "react-native";
+
+
+const BenchmarkComponent = () => {
+  return <Animated.View />;
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <BenchmarkCheck component={BenchmarkComponent} />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const BenchmarkCheck = ({ component }) => {
+  const ref = React.useRef();
+
+  const handleComplete = React.useCallback((results) => {
+    let a = results;
+    delete a.samples;
+    console.log(JSON.stringify(a));
+  }, []);
+
+  const handleStart = () => {
+    ref.current.start();
+  };
+
+  return (
+    <View style={{ maxWidth: 400, alignSelf: "center" }}>
+      <Button title="Run" onPress={handleStart} />
+      <Benchmark
+        component={component}
+        //	componentProps={}
+        onComplete={handleComplete}
+        ref={ref}
+        samples={100}
+        timeout={10000}
+        type={"mount"}
+      
+      />
+    </View>
+  );
+};
